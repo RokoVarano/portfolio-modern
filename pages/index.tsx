80f0e43f74d849ea12ebea6a5bf9101fdb77ed69
Rodrigo <rodrigo.ibaceta01@gmail.com>
2022-02-15 15:34:35 -0300

    basic positioning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import AboutMe from '../components/AboutMe'
 import BlurryBG from '../components/BlurryBG'
 import PhoneMenu from '../components/PhoneMenu'
 import PhoneMenuButton from '../components/PhoneMenuButton'
@@ -7,7 +8,8 @@ import Section from '../components/Section'
 const Home: NextPage = () => {
   const colors = {
     salmon: 'rgba(250, 128, 114, 0.5)',
-    teal: 'rgba(0, 128, 128, 0.5)'
+    teal: 'rgba(0, 128, 128, 0.5)',
+    orange: 'rgb(252, 196, 76)'
   }
 
   return (
@@ -16,11 +18,11 @@ const Home: NextPage = () => {
       <BlurryBG/>
       <PhoneMenu/>
       <div className="indexcontainer">
-        <Section id="aboutMe" bgColor={colors.salmon}><></></Section>
+        <Section id="aboutMe" bgColor={colors.orange}><AboutMe/></Section>
         <Section id="projects" bgColor={colors.teal}><></></Section>
-        <Section id="certifications" bgColor={colors.salmon}><></></Section>
+        <Section id="certifications" bgColor={colors.orange}><></></Section>
         <Section id="contactMe" bgColor={colors.teal}><></></Section>
-        <Section id="resume" bgColor={colors.salmon}><></></Section>
+        <Section id="resume" bgColor={colors.orange}><></></Section>
       </div>
     </div>
   )
