1776be578d21eca267697d3d500ae2986c40fc7d
Rodrigo <rodrigo.ibaceta01@gmail.com>
2022-02-16 14:41:11 -0300

    add second folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,8 @@ const Home: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStaticPr
       <BlurryBG/>
       <PhoneMenu/>
       <div className="indexcontainer">
-        <Toy fallTime={10} fallDistance={139} size={5} iconClass='fas fa-star' left={80} wait={0} color={colors.orange} opacity={1}/>
+        <Toy fallTime={10} fallDistance={144} size={5} iconClass='fas fa-star' left={80} wait={0} color={colors.orange} opacity={1}/>
+        <Toy fallTime={12} fallDistance={189} size={5} iconClass='fas fa-star' left={68} wait={0} color={colors.orange} opacity={1}/>
         <Toy fallTime={20} fallDistance={220} size={5} iconClass='fas fa-star' left={30} wait={1} color={colors.orange} opacity={0.5}/>
         <Toy fallTime={30} fallDistance={320} size={15} iconClass='fas fa-star' left={40} wait={2} color={colors.teal} opacity={0.5}/>
         <Toy fallTime={40} fallDistance={420} size={5} iconClass='fas fa-star' left={50} wait={3} color={colors.orange} opacity={0.5}/>
@@ -31,6 +32,7 @@ const Home: NextPage = ({ projects }: InferGetStaticPropsType<typeof getStaticPr
         <Section id="projects">
           <div style={{height: '10vh'}}>{' '}</div>
           <Projects color={colors.teal} data={projects[0]}/>
+          <Projects color={colors.salmon} data={projects[1]} left/>
         </Section>
         <Section id="certifications"><></></Section>
         <Section id="contactMe"><></></Section>
@@ -49,6 +51,13 @@ const mockData = {
         description: 'An API based TV guide for shark related shows',
         tryit: 'https://rokovarano.github.io/API-based-webapp/',
         github: 'https://github.com/RokoVarano/API-based-webapp',
+      },
+      {
+        title: 'Convention site',
+        image: 'https://github.com/RokoVarano/Assessment/blob/develop/main-desktop.png?raw=true',
+        description: 'A pure HTML/CSS site for an imaginary convention',
+        tryit: 'https://rokovarano.github.io/Assessment/',
+        github: 'https://github.com/RokoVarano/Assessment/tree/develop',
       }
     ]
   }
